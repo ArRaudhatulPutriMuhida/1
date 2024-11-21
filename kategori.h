f89b600ecd9252c6e5365f2bc08a8dd11b5237e1
ArRaudhatulPutriMuhida <putri.muhida@gmail.com>
2024-11-21 18:42:52 +0700

    kategori.h

---
+++ kategori.h
@@ -0,0 +1,11 @@
+#ifndef KATEGORI_H
+#define KATEGORI_H
+
+void menuKategori();
+void tambahKategori();
+void lihatKategori();
+int getKategoriCount(); // Fungsi untuk mendapatkan jumlah kategori
+void getKategoriList(
+    char categories[][50]); // Fungsi untuk mendapatkan daftar kategori
+
+#endif
