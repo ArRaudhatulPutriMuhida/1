83474f56b5f3380b33266417cc11496355a2a864
ArRaudhatulPutriMuhida <putri.muhida@gmail.com>
2024-11-21 18:46:28 +0700

    member.h

---
+++ member.h
@@ -0,0 +1,10 @@
+#ifndef MEMBER_H
+#define MEMBER_H
+
+// Deklarasi fungsi untuk menu kelola member
+void menuMember();       // Menampilkan menu kelola member
+void tambahMember();     // Menambahkan member baru
+void tambahPoinMember(); // Menambahkan poin ke member
+void lihatMember();      // Melihat daftar member
+
+#endif
