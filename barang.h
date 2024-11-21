02106cb826102496c536ffd0f8099d4f05d4849a
ArRaudhatulPutriMuhida <putri.muhida@gmail.com>
2024-11-21 18:33:25 +0700

    barang.h

--- barang.h
+++ barang.h
@@ -1,1 +1,13 @@
-dfdg
+#ifndef BARANG_H
+#define BARANG_H
+
+void menuBarang();
+void tambahBarang();
+void hapusBarang();
+void tambahStokBarang();
+void kurangiStokBarang();
+void lihatBarang();
+float getHargaBarang(char *nama);
+
+#endif // BARANG_H
+
